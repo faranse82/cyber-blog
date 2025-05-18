734ea06ad27fca4bc238a999053a52a8e4fce738
Faran Sepehri <faransepehri1382@gmail.com>
2025-05-18 22:31:39 +1000

    started setting up main and added logger wrapper

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,25 @@
 use actix_cors::Cors;
-use actix_web::{App, HttpServer};
+use actix_web::{middleware, web, App, HttpServer};
+use dotenv::dotenv;
+use std::env;
+
+mod db;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Starting server at http://0.0.0.0:8080");
-
-    HttpServer::new(|| {
+    dotenv().ok();
+    env_logger::init_from_env(env_logger::Env::default().filter("info"));
+    db::init_pool()
+        .await
+        .expect("failted to initialise database pool");
+    HttpServer::new(move || {
         let cors = Cors::permissive();
 
-        App::new().wrap(cors)
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .wrap(middleware::Logger::default())
+            .wrap(cors)
     })
     .bind("0.0.0.0:8080")?
     .run()
